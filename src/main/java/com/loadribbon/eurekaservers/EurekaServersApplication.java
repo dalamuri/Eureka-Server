e10bbe0aa383bf7c7d9c43471eb97a45e0b146a0
sougandh <dalamuri09@gmail.com>
2018-01-05 12:49:51 -0500

    first commit

---
+++ src/main/java/com/loadribbon/eurekaservers/EurekaServersApplication.java
@@ -0,0 +1,12 @@
+package com.loadribbon.eurekaservers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServersApplication.class, args);
+	}
+}
